f273936d29f410719fcb76caab7213586d932db8
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-04-11 22:36:41 -0500

    Move headers to Astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
 	output: "server",
+	headers: {
+		"Access-Control-Allow-Origin": "*"
+	},
 	adapter: netlify({
 		builders: true
 	})
