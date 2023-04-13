fdd734c82fa9f48c6c2ca9d0a1aaf46b39ace258
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-04-13 15:02:31 -0500

    Add allow methods to edge function

--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -12,5 +12,7 @@ export default async (request, context) => {
 	response.headers.set("X-Your-Custom-Header", "A custom value");
 	response.headers.set("Access-Control-Allow-Origin", "*");
 	response.headers.set("Cache-Control", "public, max-age=2592000, immutable");
+	response.headers.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS");
+
 	return response;
 };
