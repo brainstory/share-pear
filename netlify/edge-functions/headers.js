5b32f327af1e28dce4d96890d4267aac8b74afe5
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-04-13 14:31:57 -0500

    Add edge function for modifying headers

---
+++ netlify/edge-functions/headers.js
@@ -0,0 +1,16 @@
+export default async (request, context) => {
+	const response = await context.next();
+	// const url = new URL(request.url);
+
+	// if (url.pathname.startsWith("/_astro")) {
+	// 	response.headers.set(
+	// 		"Cache-Control",
+	// 		"public, max-age=31536000, immutable"
+	// 	);
+	// }
+
+	response.headers.set("X-Your-Custom-Header", "A custom value");
+	response.headers.set("Access-Control-Allow-Origin", "*");
+	response.headers.set("Cache-Control", "public, max-age=2592000, immutable");
+	return response;
+};
