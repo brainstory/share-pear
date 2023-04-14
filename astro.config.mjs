8a888ee0640762672ee8a21f89d63e81b68313e3
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-04-13 19:48:25 -0500

    Remove unused headers settings, remove comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,6 @@ import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
 	output: "server",
-	server: {
-		headers: {
-			"Access-Control-Allow-Origin": "*"
-		}
-	},
 	adapter: netlify({
 		builders: true
 	})
--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -1,6 +1,5 @@
 export default async (request, context) => {
 	const response = await context.next();
-	// const url = new URL(request.url);
 
 	if (request.method === "OPTIONS") {
 		return new Response("ok", {
@@ -12,21 +11,6 @@ export default async (request, context) => {
 		});
 	}
 
-	// if (url.pathname.startsWith("/_astro")) {
-	// 	response.headers.set(
-	// 		"Cache-Control",
-	// 		"public, max-age=31536000, immutable"
-	// 	);
-	// }
-
-	// return new Response("OK", {
-	// 	headers: {
-	// 		"Access-Control-Allow-Origin": "*",
-	// 		"Access-Control-Allow-Headers": "Content-Type, markdown",
-	// 		"X-Your-Custom-Header": "A custom value for testing"
-	// 	}
-	// });
-
 	response.headers.set("Access-Control-Allow-Origin", "*");
 	response.headers.set(
 		"Access-Control-Allow-Headers",
@@ -34,8 +18,6 @@ export default async (request, context) => {
 	);
 	response.headers.set("Cache-Control", "public, max-age=2592000, immutable");
 	response.headers.set("X-Your-Custom-Header", "A custom value for testing");
-
-	console.log(response);
 
 	return response;
 };
