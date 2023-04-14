9c88b5f0bd48d1eeafc13d3b4bad892f4ded406f
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-04-13 21:04:17 -0500

    Add CORS headers to dynamic pages (#1)
    * Add CORS headers to dynamic pages
    
    * Move headers to Astro config file
    
    * Add headers to Netlify TOML
    
    * Fix typo because I am the worst
    
    * Make all URLs accept headers
    
    * Add a cache control header to the /share/ route
    
    * Update Astro packages and server headers
    
    * Add edge function for modifying headers
    
    * Add allow methods to edge function
    
    * Force response to be a 200
    
    * Remove manual status setting, log response
    
    * Change how response is returned
    
    * Actually name the header correctly
    
    * Add custom headers to access control
    
    * Move response headers out of object
    
    * Try redirect proxy
    
    * Add condition for options preflight
    
    * Add decode URI component to markdown passed in
    
    * Remove debugging prints
    
    * Remove unused headers settings, remove comments
    
    * Update Astro
    
    * Add allowed domains
    
    * Change allowed URLs to regex
    
    * Comment out URL crap until later
    
    * Add more checking of the markdown for empty content

---
+++ netlify/edge-functions/headers.js
@@ -0,0 +1,34 @@
+export default async (request, context) => {
+	const response = await context.next();
+
+	if (request.method === "OPTIONS") {
+		return new Response("ok", {
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Headers": "Content-Type, markdown",
+				"X-Your-Custom-Header": "A custom value for testing"
+			}
+		});
+	}
+
+	// TODO: only allow requests from our own domains
+	// let url = new URL(request.url);
+
+	// let allowedUrlsRegex =
+	// 	/^(https?:\/\/)?(localhost(:\d+)?|([a-zA-Z0-9-]+\.)?contenda\.co|(contenda(-test)?-platty-plat)\.netlify\.app)(\/.*)?$/;
+
+	// if (url && allowedUrlsRegex.test(url)) {
+	// 	response.headers.set("Access-Control-Allow-Origin", origin);
+	// }
+
+	response.headers.set("Access-Control-Allow-Origin", "*");
+
+	response.headers.set(
+		"Access-Control-Allow-Headers",
+		"Content-Type, markdown"
+	);
+	response.headers.set("Cache-Control", "public, max-age=2592000, immutable");
+	response.headers.set("X-Your-Custom-Header", "A custom value for testing");
+
+	return response;
+};
