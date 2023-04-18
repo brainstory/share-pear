eef9baff04361ed273f0a6df4ca1bbccee927c0b
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-04-18 13:29:50 -0500

    Revert "Add current URL to headers"
    This reverts commit 7fd4eef4bbc9cf2855c52f651a761bdf6829331b.

--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -21,8 +21,6 @@ export default async (request, context) => {
 		response.headers.set("Access-Control-Allow-Origin", url);
 	}
 
-	response.headers.set("Access-Control-Allow-Origin", response.url);
-
 	// Accept markdown header
 	response.headers.set(
 		"Access-Control-Allow-Headers",
