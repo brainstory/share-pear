ddfb066c6c4e41cd489a1aef89db81e0af9551e5
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2024-01-24 00:11:32 -0600

    Fix headers

--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -4,18 +4,8 @@ export default async (request, context) => {
 	// Only allow requests from our own domains
 	let url = request.headers.get("origin");
 
-	// Allowed URLs are:
-	// localhost
-	// contenda.co
-	// contenda.netlify.app
-	// contenda-platty-plat.netlify.app
-	// contenda-test-platty-plat.netlify.app
-	// brainstory.ai
-	// brainstory.netlify.app
-	// test.brainstory.ai
-
 	let allowedUrlsRegex =
-		/^(https?:\/\/)?(localhost|contenda\.co|contenda\.netlify\.app|contenda-platty-plat\.netlify\.app|contenda-test-platty-plat\.netlify\.app|brainstory\.ai|brainstory\.netlify\.app|test\.brainstory\.ai)(:[0-9]+)?$/;
+		/^(https?:\/\/)?(localhost(:\d+)?|([a-zA-Z0-9-]+\.)?contenda\.co|contenda\.netlify\.app|contenda-platty-plat\.netlify\.app|contenda-test-platty-plat\.netlify\.app|([a-zA-Z0-9-]+\.)?brainstory\.ai|brainstory\.netlify\.app)(:[0-9]+)?$/;
 
 	// Handle preflight requests
 	if (request.method === "OPTIONS" && allowedUrlsRegex.test(url)) {
