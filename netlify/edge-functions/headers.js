ace795b07769065ee379d5e083a7827733cdc1db
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2024-01-23 23:30:07 -0600

    Brainstory-ify

--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -4,8 +4,18 @@ export default async (request, context) => {
 	// Only allow requests from our own domains
 	let url = request.headers.get("origin");
 
+	// Allowed URLs are:
+	// localhost
+	// contenda.co
+	// contenda.netlify.app
+	// contenda-platty-plat.netlify.app
+	// contenda-test-platty-plat.netlify.app
+	// brainstory.ai
+	// brainstory.netlify.app
+	// test.brainstory.ai
+
 	let allowedUrlsRegex =
-		/^(https?:\/\/)?(localhost(:\d+)?|([a-zA-Z0-9-]+\.)?contenda\.co|(contenda(-test)?-platty-plat)\.netlify\.app)(\/.*)?$/;
+		/^(https?:\/\/)?(localhost|contenda\.co|contenda\.netlify\.app|contenda-platty-plat\.netlify\.app|contenda-test-platty-plat\.netlify\.app|brainstory\.ai|brainstory\.netlify\.app|test\.brainstory\.ai)(:[0-9]+)?$/;
 
 	// Handle preflight requests
 	if (request.method === "OPTIONS" && allowedUrlsRegex.test(url)) {
